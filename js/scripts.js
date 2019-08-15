e98e507ebd714adb2cb11d3f45eb5f5deed6f590
jennifer batara and micah zaayer <jennifer@>
2019-08-15 10:28:30 -0700

    hide animal text when another button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,21 @@ $(document).ready(function(){
   $(".btn").click(function(event){
     if(event.target.id === "simpona-btn"){
       $("#secret-simpona").toggle();
+      $("#secret-lemur").hide();
+      $("#secret-sloth").hide();
+
     }
     else if(event.target.id === "lemur-btn"){
       $("#secret-lemur").toggle();
+      $("#secret-simpona").hide();
+      $("#secret-sloth").hide();
+
     }
     else{
       $("#secret-sloth").toggle();
+      $("#secret-lemur").hide();
+      $("#secret-simpona").hide();
+
     }
   });
 });
